5d79b916217647a288d0e12b71051fd608a87d07
Rayane <rayane.haroun.off@gmail.com>
2023-04-24 22:03:12 +0200

    Transfere du projet git lab sur git hub

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=lisaood
+export POSTGRES_PASSWORD=lisa1999
+export POSTGRES_HOST=postgreSql
+export POSTGRES_DB=pipeline
+export AWS_BUCKET=projet-pipeline
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
